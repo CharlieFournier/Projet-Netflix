bc0e478e4268f77e3e6fe42cf62e2a0978f858d8
ICodder <you@example.com>
2023-10-31 10:53:00 -0400

    mise a jour bd personnages

--- Netflix/database/migrations/2023_10_27_113335_create_films_table.php
+++ Netflix/database/migrations/2023_10_27_113335_create_films_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('films', function (Blueprint $table) {
             $table->id();
             $table->string('titre',100);
-            $table->string('resumer', '2000');
+            $table->string('resumer', '500');
             $table->integer('duree');//tout en minute convertir avec modulo 60.
             $table->year('annee');
 
--- Netflix/database/seeders/PersonnesSeeder.php
+++ Netflix/database/seeders/PersonnesSeeder.php
@@ -26,6 +26,54 @@ class PersonnesSeeder extends Seeder
                 'nom' => 'Damien Chazelle',
                 'date' => '1985-01-19',
                 'photo' => 'https://th.bing.com/th/id/R.e77a86aa90885692e578f071d635378a?rik=Mu5RwzlsyLFj%2fQ&riu=http%3a%2f%2fbr.web.img3.acsta.net%2fpictures%2f18%2f07%2f26%2f21%2f58%2f5869922.jpg&ehk=zID%2bH9z0VxLqG%2fDanmRGFtRD8wpiNw4Wuq78AT3Gprw%3d&risl=&pid=ImgRaw&r=0'
+            ],
+            [
+                'id' => 3,
+                'nom' => 'Christopher Nolan',
+                'date' => '1970-07-30',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 4,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 5,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 6,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 7,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 8,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 9,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
+            ],
+            [
+                'id' => 10,
+                'nom' => 'Edgar Wright',
+                'date' => '1974-04-18',
+                'photo' => 'https://th.bing.com/th/id/OIP.t_V68JZejEfG8A-QK7D97wHaE7?pid=ImgDet&rs=1'
             ]
         ]);  
     }
