3f0c705d8f42243ba0926fc0e098271aeefff169
ICodder <charliefournier01@hotmail.com>
2023-11-20 18:20:30 -0500

    mise a jour d'infos concernant les genres

--- Netflix/routes/web.php
+++ Netflix/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmsController;
 use App\Http\Controllers\PersonnesController;
+use App\Http\Controllers\GenresController;
 
 /*
 |--------------------------------------------------------------------------
